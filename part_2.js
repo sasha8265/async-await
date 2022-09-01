4e0b682958020997b6c1b3e3eb7daba3da8cb8cd
Sasha Czerniawski <sashacandoit@gmail.com>
2022-09-01 06:30:03 -0400

    question 2 done

--- part_2.js
+++ part_2.js
@@ -18,22 +18,22 @@ newDeck()
 
 // 2. Make a request to the deck of cards API to request a single card from a newly shuffled deck. Once you have the card, make a request to the same API to get one more card from the same deck. Once you have both cards, console.log the values and suits of both cards.
 
-async function newDeck() {
+async function twoCards() {
     let shuffledDeck = await axios.get('http://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
 
     deckId = shuffledDeck.data.deck_id
+    
+    let card1 = await axios.get(`http://deckofcardsapi.com/api/deck/${deckId}/draw/`)
+    card1 = card1.data.cards[0]
 
-    function drawCard() {
-        let drawCard = await axios.get(`http://deckofcardsapi.com/api/deck/${deckId}/draw/`)
-
-        return drawCard.data.cards[0]
-    }
-
-    let card1 = drawCard()
-    let card2 = drawCard()
+    let card2 = await axios.get(`http://deckofcardsapi.com/api/deck/${deckId}/draw/`)
+    card2 = card2.data.cards[0]
+    
     let cards = [card1, card2]
 
     cards.forEach(function (card) {
         console.log(`${card.value} of ${card.suit}`);
     });
 }
+
+twoCards()
